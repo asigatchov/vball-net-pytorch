dc46321a78144f16f1ea4088b356c38b566c898d
Alexandr Sigatchov <sa@inventos.ru>
2026-03-16 10:13:48 +0000

    tmp

--- run_gru.sh
+++ run_gru.sh
@@ -3,14 +3,14 @@
 # Training script for VballNetV1c (GRU model) with 15 grayscale frames
 
 # Set data paths
-DATADIR=datasets/mix-vb/train_preprocessed
-VAL_DATADIR=datasets/mix-vb/test_preprocessed
-
-DATADIR=../TrackNetV4-PyTorch/datasets/mix_volleyball_preprocessed
-VAL_DATADIR=../TrackNetV4-PyTorch/datasets/mix_volleyball_test_preprocessed
-
-# DATADIR=/home/nssd/gled/vb/dataset-vb/badminton/train_preprocessed
-# VAL_DATADIR=/home/nssd/gled/vb/dataset-vb/badminton/test_preprocessed
+#DATADIR=datasets/mix-vb/train_preprocessed
+#VAL_DATADIR=datasets/mix-vb/test_preprocessed
+#
+#DATADIR=../TrackNetV4-PyTorch/datasets/mix_volleyball_preprocessed
+#VAL_DATADIR=../TrackNetV4-PyTorch/datasets/mix_volleyball_test_preprocessed
+#
+DATADIR=./datasets/train_preprocessed
+VAL_DATADIR=./datasets/test_preprocessed
 # Run training
 uv run  src/train_gru.py \
   --data "$DATADIR" \
@@ -24,6 +24,6 @@ uv run  src/train_gru.py \
   --batch 16  \
   --scheduler ReduceLROnPlateau \
   --workers 8 \
-  --resume ./outputs/VballNetV1c_seq15_grayscale_badminton/checkpoints/VballNetV1c_seq15_grayscale_best.pth
+#  --resume ./outputs/VballNetV1c_seq15_grayscale_badminton/checkpoints/VballNetV1c_seq15_grayscale_best.pth
 
 echo "Training completed!"
